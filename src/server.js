67348a395ba6611027ebf838deea931e2e52a4c2
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-02 11:59:57 +0900

    default Socket.io

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { join } from "path";
+import socketIo from "socket.io";
+import logger from "morgan";
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.get("/", (req, res) => res.render("home"));
+app.use(express.static(join(__dirname, "static")));
+
+const handleListener = () => {
+  console.log(`✅ Server Running : http://localhost:${PORT}`);
+};
+
+const server = app.listen(PORT, handleListener);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  setTimeout(() => socket.emit("hello"), 5000);
+});
---
+++ src/static/index.js
@@ -0,0 +1,3 @@
+const socket = io("/");
+
+socket.on("hello", () => console.log("Somebody said Hello"));
